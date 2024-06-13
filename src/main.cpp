08c0c7273a8120dbd742b1e369497a3bd62f3430
DFVSQY <dfvsqy@qq.com>
2024-06-13 13:41:12 +0800

    创建一个简单的窗口并填充帧缓冲区颜色

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include <cstdio>
 #include <gl/GL.h>
+#include <stdio.h>
 
 // 错误回调函数
 void glfw_error_callback(int error, const char *description)
@@ -17,6 +19,12 @@ void glfw_key_callback(GLFWwindow *window, int key, int scancode, int action, in
     }
 }
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    printf("Framebuffer size changed: %d, %d\n", width, height);
+    glViewport(0, 0, width, height);
+}
+
 int main()
 {
     // 设置错误回调函数
@@ -28,8 +36,16 @@ int main()
         return -1;
     }
 
+    // 创建窗口前设置一些窗口选项值（使用OpenGL3.3版本，并且使用核心配置模式）
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // 定义窗口的宽高
+    int win_width = 800, win_height = 600;
+
     // 创建window窗口以及OpenGL上下文
-    GLFWwindow *window = glfwCreateWindow(640, 480, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(win_width, win_height, "LearnOpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -39,13 +55,28 @@ int main()
     // 设置为当前上下文
     glfwMakeContextCurrent(window);
 
+    // 使用GLFW库来获取OpenGL函数的地址，并使用glad库来加载这些地址到相应的函数指针中，
+    // 要在绑定了当前上下文后才能调用该函数
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwTerminate();
+        return -1;
+    }
+
     // 设置键盘输入回调函数
     glfwSetKeyCallback(window, glfw_key_callback);
 
+    // 当窗口改变时，在回调函数里面重新设置视口大小
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 定义视口的宽高，铺满整个窗口
+    glViewport(0, 0, win_width, win_height);
+
     while (!glfwWindowShouldClose(window))
     {
-        // 清理缓冲区
-        glClear(GL_COLOR_BUFFER_BIT);
+        // 清理缓冲区并设置为指定的颜色
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 状态值设置，用于指定颜色值
+        glClear(GL_COLOR_BUFFER_BIT);         // 状态值应用，清理掉颜色缓冲区并设置为指定的颜色
 
         // 交换缓冲区，显示图像
         glfwSwapBuffers(window);
