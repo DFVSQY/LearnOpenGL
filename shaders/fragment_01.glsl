fe1a44bfb46f833e81fceeb377855553337f6e0e
DFVSQY <dfvsqy@qq.com>
2024-06-19 20:30:53 +0800

    顶点着色器和像素着色器之间数据的传递示例

--- shaders/fragment_01.glsl
+++ shaders/fragment_01.glsl
@@ -8,7 +8,15 @@
 */
 out vec4 FragColor;
 
+/*
+ * 在顶点着色器中声明了一个顶点颜色变量作为vec4输出，并在顶点着色器中设置。
+ * 在片段着色器中声明了一个类似的顶点颜色输入，由于它们具有相同的类型和名称，因此片段着色器中的顶点颜色与顶点着色器中的顶点颜色相关联。
+*/
+in vec4 vertexColor;
+
 void main()
 {
-    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+
+    FragColor = vertexColor;
 }
