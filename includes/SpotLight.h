f3c80fa6e72c5bb60307688eb3616cc8ad2def74
DFVSQY <dfvsqy@qq.com>
2024-07-26 01:20:32 +0800

    完善聚光灯类

--- includes/SpotLight.h
+++ includes/SpotLight.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #include "BaseLight.h"
+#include "glad/glad.h"
 
 class SpotLight : public BaseLight
 {
   protected:
-    glm::vec3 m_position;
+    glm::vec3 m_position;  // 灯光位置
+    glm::vec3 m_direction; // 灯光方向
+    GLfloat m_cutOff;      // 切光角
 
   public:
-    SpotLight(const glm::vec3 &position, const glm::vec3 &ambient, const glm::vec3 &diffuse, const glm::vec3 &specular);
+    SpotLight(const glm::vec3 &position, const glm::vec3 &direction, const GLfloat cutOff, const glm::vec3 &ambient,
+              const glm::vec3 &diffuse, const glm::vec3 &specular);
     ~SpotLight();
 };
--- src/SpotLight.cpp
+++ src/SpotLight.cpp
@@ -2,9 +2,9 @@
 #include "BaseLight.h"
 #include "glm/fwd.hpp"
 
-SpotLight::SpotLight(const glm::vec3 &position, const glm::vec3 &ambient, const glm::vec3 &diffuse,
-                     const glm::vec3 &specular)
-    : BaseLight(ambient, diffuse, specular), m_position(position)
+SpotLight::SpotLight(const glm::vec3 &position, const glm::vec3 &direction, const GLfloat cutOff,
+                     const glm::vec3 &ambient, const glm::vec3 &diffuse, const glm::vec3 &specular)
+    : BaseLight(ambient, diffuse, specular), m_position(position), m_direction(direction), m_cutOff(cutOff)
 {
 }
 
