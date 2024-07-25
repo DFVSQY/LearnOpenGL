a7c477c1e0fa37f10b428b4c3519f0b8f322e86b
DFVSQY <dfvsqy@qq.com>
2024-07-25 22:40:36 +0800

    完善点光源类

--- includes/PointLight.h
+++ includes/PointLight.h
@@ -1,14 +1,19 @@
 #pragma once
 
 #include "BaseLight.h"
+#include "glad/glad.h"
 
 class PointLight : public BaseLight
 {
   protected:
     glm::vec3 m_position;
 
+    GLfloat m_constant;  // 常量系数
+    GLfloat m_linear;    // 一次递减系数
+    GLfloat m_quadratic; // 二次递减系数
+
   public:
-    PointLight(const glm::vec3 &position, const glm::vec3 &ambient, const glm::vec3 &diffuse,
-               const glm::vec3 &specular);
+    PointLight(const glm::vec3 &position, const GLfloat linear, const GLfloat quadratic, const glm::vec3 &ambient,
+               const glm::vec3 &diffuse, const glm::vec3 &specular, const GLfloat constant);
     ~PointLight();
 };
--- src/PointLight.cpp
+++ src/PointLight.cpp
@@ -1,9 +1,11 @@
 #include "PointLight.h"
 #include "BaseLight.h"
 
-PointLight::PointLight(const glm::vec3 &position, const glm::vec3 &ambient, const glm::vec3 &diffuse,
-                       const glm::vec3 &specular)
-    : BaseLight(ambient, diffuse, specular), m_position(position)
+PointLight::PointLight(const glm::vec3 &position, const GLfloat linear, const GLfloat quadratic,
+                       const glm::vec3 &ambient, const glm::vec3 &diffuse, const glm::vec3 &specular,
+                       const GLfloat constant = 1.0f)
+    : BaseLight(ambient, diffuse, specular), m_position(position), m_constant(constant), m_linear(linear),
+      m_quadratic(quadratic)
 {
 }
 
